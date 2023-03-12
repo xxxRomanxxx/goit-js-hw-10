8d1ec66ba26db6e9ad0fd4fa680f34746cdbc6a3
xxxRomanxxx <112776664+xxxRomanxxx@users.noreply.github.com>
2023-03-12 19:32:09 +0200

    Start

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+
+import Notiflix from 'notiflix';
+
 
 const DEBOUNCE_DELAY = 300;
+
+const inputCountry = document.querySelector('input');
+
+inputCountry.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
+
+function onInputSearch(e) {
+    const countryName = inputCountry.value.trim();
+    console.log(countryName);
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`).then(r => r.json()).then(
+    data => { 
+    
+        if (data.length > 10) {
+            
+            Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+        } else if (data.length > 2 && data.length < 10) {
+            console.log('від 2 до 10');
+            data.map((item) => {
+            console.log(item)
+            })
+        }
+         else if (data.length === 1) data.map((item) => {
+            console.log(item)
+            });
+    })
+    
+}
+
+function fetchCountry(name) {
+    
+}
